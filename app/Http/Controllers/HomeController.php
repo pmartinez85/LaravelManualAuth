48856c2ab4f20071c266724b3ec8c7adcb40c82f
Pedro Martinez <pmartinez1085@gmail.com>
2016-09-27 21:41:28 +0200

    new controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,19 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 use App\Http\Requests;
+
 
 class HomeController extends Controller
 {
-    /**
-     * @param array $middleware
-     */
+
     public function home()
     {
        // $data = ['username' => 'nommm'];
        // return view('home', $data);
-        return view('home') ->with  ('username', 'amparo');
+        return view('home')->with  ('username', 'amparo');
     }
 }
