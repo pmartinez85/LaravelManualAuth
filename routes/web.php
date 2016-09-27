b76ab782a8b92b67df925d5fa49b9a143f0cfcea
Pedro Martinez <pmartinez1085@gmail.com>
2016-09-27 20:56:11 +0200

    vistes

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+Route::get('/register', function () {
+    return view('register');
+});
