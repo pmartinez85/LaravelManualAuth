2e73708a795f3f1c7c15e4575a752255d53e1184
Pedro Martinez <pmartinez1085@gmail.com>
2016-10-11 20:19:54 +0200

    primer intent query string

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,14 +9,24 @@ class HomeController extends Controller
 {
     public function __construct(){
 
-        $this->middleware('auth');
+        //$this->middleware('auth');
 }
 
     public function home()
     {
-        $user = Auth::user();
+      //  $user = Auth::user();
+            //ESTAT SESSIO
+        $user = $this->getUser();
         return view('home')
             ->withUser($user);
 
     }
+
+    private function getUser(){
+
+        //Opcio 1 : Query string $_GET
+        return $_GET['user'];
+    }
+
+
 }
