5f76428f618b87ab992efff0e0591b22c5509e53
Pedro Martinez <pmartinez1085@gmail.com>
2016-10-18 20:33:24 +0200

    aplciar css

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,16 +6,37 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="css/app.css">
 </head>
 <body>
 Login form here!
-<form action="/login" method="POST">
-    {{csrf_field()}}
-    Usuari : <input type="text" name="user">
-    Password : <input type="text" name="pass">
-        <input type="submit" value="Accepta">
-
-
+<form class="form-horizontal">
+    <div class="form-group">
+        <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+        <div class="col-sm-10">
+            <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+        </div>
+    </div>
+    <div class="form-group">
+        <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+        <div class="col-sm-10">
+            <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+            <div class="checkbox">
+                <label>
+                    <input type="checkbox"> Remember me
+                </label>
+            </div>
+        </div>
+    </div>
+    <div class="form-group">
+        <div class="col-sm-offset-2 col-sm-10">
+            <button type="submit" class="btn btn-default">Sign in</button>
+        </div>
+    </div>
 </form>
 </body>
 </html>
