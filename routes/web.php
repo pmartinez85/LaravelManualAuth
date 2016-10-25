a9c98e39edb9823909876c3137f62bcf27d69ea6
Pedro Martinez <pmartinez1085@gmail.com>
2016-10-25 19:42:38 +0200

    neteja pre-middleware

--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,42 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+//PAS 1 Middleware. com protegir pàgines?
+
+
+
+Route::group(['middleware' => ['manualauth']], function () {
+
+    Route::get('/tasques', function () {
+        return view('tasques');
+    });
+});
+
+
+
+
+
+
+
+
+
 //Route::get('/home', function () {
 //    return view('home');
 //});
 
-Auth::loginUsingId(4);
-
-
-Route::get('/login', 'LoginController@login');
-Route::get('/login', 'LoginController@showLoginForm');
-Route::post('/login', 'LoginController@postlogin');
+//Auth::loginUsingId(4);
+//
+//
+//Route::get('/login', 'LoginController@login');
+//Route::get('/login', 'LoginController@showLoginForm');
+//Route::post('/login', 'LoginController@postlogin');
 
 
 //Route::get('/register', 'RegisterController@register');
 
 
-Route::get('/home', 'HomeController@home');
+//Route::get('/home', 'HomeController@home');
 
 
 //
